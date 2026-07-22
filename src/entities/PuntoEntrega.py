946bccca540efc0aaf30e1e509668fb1762a05c0
DESKTOP-7ST3MPC\Janus <mafe.palacio53@gmail.com>
2026-07-22 02:53:53 -0500

    Correccion de una variable mal escrita

--- src/entities/PuntoEntrega.py
+++ src/entities/PuntoEntrega.py
@@ -47,10 +47,10 @@ class PuntoEntrega(Base):
 
     fecha_creacion = Column(DateTime(timezone=True), server_default=func.now())
     fecha_edicion = Column(DateTime(timezone=True), onupdate=func.now())
-    id_usuario_crea = Column(
+    usuario_crea_id = Column(
         UUID(as_uuid=True), ForeignKey("usuarios.usuario_id"), nullable=True
     )
-    id_usuario_edita = Column(
+    usuario_edita_id = Column(
         UUID(as_uuid=True), ForeignKey("usuarios.usuario_id"), nullable=True
     )
 
